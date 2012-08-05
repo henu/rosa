d3c11ea22e29422df991477bdf842651314c2ac4
Henrik Heino <henrik.heino@gmail.com>
2012-08-05 18:53:53 +0300

    FsMetadata works now.

--- src/nodes/filesysteminfo.h
+++ src/nodes/filesysteminfo.h
@@ -3,6 +3,10 @@
 
 #include <hpp/path.h>
 #include <hpp/bytev.h>
+#include <hpp/serialize.h>
+#include <hpp/time.h>
+#include <map>
+#include <string>
 
 namespace Nodes
 {
@@ -13,27 +17,69 @@ namespace Nodes
 		FSTYPE_SYMLINK = 2
 	};
 
-// TODO: Implement this!
-	struct FsMetadata
+	class FsMetadata
 	{
+
+	public:
+
 		inline FsMetadata(void)
 		{
+			Hpp::Time now = Hpp::now();
+			pairs["crtime"] = Hpp::sizeToStr(now.getSeconds());
+			pairs["crtime_nsec"] = Hpp::sizeToStr(now.getNanoseconds());
+			pairs["mtime"] = Hpp::sizeToStr(now.getSeconds());
+			pairs["mtime_nsec"] = Hpp::sizeToStr(now.getNanoseconds());
 		}
 		inline FsMetadata(Hpp::Path const& path)
 		{
-(void)path;
+			// Read metadata from given target
+			Hpp::Path::Metadata metadata = path.getMetadata();
+
+			// User permissions
+			if (!metadata.owner.empty()) pairs["user"] = metadata.owner;
+			if (!metadata.group.empty()) pairs["group"] = metadata.group;
+
+			// Creation and modification time
+			if (metadata.created.getSeconds() != 0) {
+				pairs["crtime"] = Hpp::sizeToStr(metadata.created.getSeconds());
+				pairs["crtime_nsec"] = Hpp::sizeToStr(metadata.created.getNanoseconds());
+			}
+			if (metadata.modified.getSeconds() != 0) {
+				pairs["mtime"] = Hpp::sizeToStr(metadata.modified.getSeconds());
+				pairs["mtime_nsec"] = Hpp::sizeToStr(metadata.modified.getNanoseconds());
+			}
 		}
 		inline FsMetadata(Hpp::ByteV::const_iterator& serialized_it, Hpp::ByteV::const_iterator const& serialized_end)
 		{
-(void)serialized_it;
-(void)serialized_end;
+			while (serialized_it != serialized_end) {
+				std::string key = Hpp::deserializeString(serialized_it, serialized_end, 1);
+				std::string value = Hpp::deserializeString(serialized_it, serialized_end, 2);
+				pairs[key] = value;
+			}
 		}
 
 		Hpp::ByteV serialize(void) const
 		{
 			Hpp::ByteV result;
+			for (Pairs::const_iterator pairs_it = pairs.begin();
+			     pairs_it != pairs.end();
+			     ++ pairs_it) {
+				std::string const& key = pairs_it->first;
+				std::string const& value = pairs_it->second;
+				HppAssert(key.size() <= 0xff, "Too long key!");
+				HppAssert(value.size() <= 0xffff, "Too long value!");
+				Hpp::serializeString(result, key, 1);
+				Hpp::serializeString(result, value, 2);
+			}
 			return result;
 		}
+
+	private:
+
+		typedef std::map< std::string, std::string > Pairs;
+
+		Pairs pairs;
+
 	};
 
 }
