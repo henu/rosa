2026891d7e1368b3b33a2e810fc1bc177810ed31
Henrik Heino <henrik.heino@gmail.com>
2013-03-14 00:02:47 +0200

    secureRandomInt() now uses source of secure random numbers more efficiently.

--- src/misc.h
+++ src/misc.h
@@ -11,9 +11,15 @@ inline size_t secureRandomInt(size_t min, size_t max, bool secure_source)
 	if (secure_source) {
 		size_t range = max - min + 1;
 		
-		Hpp::ByteV random_bytev = Hpp::randomSecureData(8);
-		HppAssert(random_bytev.size() == 8, "Random get failed!");
-		uint64_t random_raw = Hpp::cStrToUInt64(&random_bytev[0]);
+		// Calculate how many bytes are required
+		size_t bytes_required = 1;
+		while (range > (1 << (bytes_required * 8))) {
+			++ bytes_required;
+		}
+		
+		Hpp::ByteV random_bytev = Hpp::randomSecureData(bytes_required);
+		HppAssert(random_bytev.size() == bytes_required, "Random get failed!");
+		uint64_t random_raw = Hpp::cStrToUInt(&random_bytev[0], bytes_required);
 		uint64_t random = (random_raw % range);
 		
 		return min + random;
